a687dce868ee4aeabd3f90cc17ce40929babfd0a
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2021-09-27 21:20:46 +0200

    Add solutions to problem page

--- src/components/ProblemView.tsx
+++ src/components/ProblemView.tsx
@@ -8,9 +8,29 @@ interface Problem {
   upvotes: number;
 }
 
+interface Solution {
+  title: string;
+  description: string;
+  upvotes: number;
+}
+
+const Solution = ({solution}: {solution: Solution}) => {
+  return (
+    <div style={{marginTop: 8, display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+      <div style={{marginRight: 16}}>
+        <span>⬆️</span><span>{solution.upvotes}</span>
+      </div>
+      <div>
+        <h4>{solution.title}</h4>
+        <p>{solution.description}</p>
+      </div>
+    </div>);
+};
+
 export const ProblemView = () => {
   const {problemId} = useParams<{problemId: string}>();
 
+  const [solutions, setSolutions] = useState<Solution[] | undefined>(undefined);
   const [problem, setProblem] = useState<Problem | undefined>(undefined);
 
   const getProblem = (problemId: string) => {
@@ -22,12 +42,37 @@ export const ProblemView = () => {
     });
   };
 
+  const getSolutions = (problemId: string) => {
+    // soon: firebase.get(problemId)
+    setSolutions([
+      {
+        title: "Dry sponge is a happy sponge",
+        description: "Thank you for this question. A very important part of any sponge keeping is to keep them dry. A dry sponge is a happy sponge.",
+        upvotes: 1337,
+      },
+      {
+        title: "Keep them wet",
+        description: "As long as your sponges are constantly wet they won't get any more gross.",
+        upvotes: -12,
+      },
+    ]);
+  };
+
   useEffect(() => {
     // TODO: remove this setTimeout when firebase integration is merged
     setTimeout(() =>
       getProblem(problemId)
       , 1000);
   }, [problemId]);
+
+  useEffect(() => {
+    if (problem) {
+      // TODO: remove this setTimeout when firebase integration is merged
+      setTimeout(() =>
+        getSolutions(problemId)
+        , 1000);
+    }
+  }, [problem]);
 
   if (!problem) {
     return (
@@ -38,14 +83,25 @@ export const ProblemView = () => {
     );
   } else {
     return (
-      <div className="content-wrapper" style={{display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-        <div style={{marginRight: 24, backgroundColor: colors.gold, borderRadius: 48, padding: 24, display: 'flex', flexDirection: 'row'}}>
-          <span>⬆️</span><span>{problem.upvotes}</span>
+      <div className="content-wrapper">
+        <div style={{display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+          <div style={{marginRight: 24, backgroundColor: colors.gold, borderRadius: 48, padding: 24, display: 'flex', flexDirection: 'row'}}>
+            <span>⬆️</span><span>{problem.upvotes}</span>
+          </div>
+          <div>
+            <h1>{problem.title}</h1>
+            <p>{problem.description}</p>
+          </div>
         </div>
-        <div>
-          <h1>{problem.title}</h1>
-          <p>{problem.description}</p>
-        </div>
+
+        {solutions &&
+          <div style={{marginTop: 24}}>
+            <h2>Solutions</h2>
+            {solutions.map(solution =>
+              <Solution solution={solution} />
+            )}
+          </div>
+        }
       </div>
     );
   }
