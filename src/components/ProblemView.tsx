0844915e5977ad8f105910203cb5e1a4e5edd3d9
Christoffer Tønnessen <christoffer.tonnessen@gmail.com>
2021-09-27 20:53:51 +0200

    Add upvote on problem page

--- src/components/ProblemView.tsx
+++ src/components/ProblemView.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
+import {colors} from '../colors';
 
 interface Problem {
   title: string;
   description: string;
+  upvotes: number;
 }
 
 export const ProblemView = () => {
@@ -16,6 +18,7 @@ export const ProblemView = () => {
     setProblem({
       title: "Sponge buildup",
       description: "Oftentimes my household sponges accumulate an awful amount of buildup, how can I solve this?",
+      upvotes: 29,
     });
   };
 
@@ -35,9 +38,14 @@ export const ProblemView = () => {
     );
   } else {
     return (
-      <div className="content-wrapper">
-        <h1>{problem.title}</h1>
-        <p>{problem.description}</p>
+      <div className="content-wrapper" style={{display: 'flex', flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+        <div style={{marginRight: 24, backgroundColor: colors.gold, borderRadius: 48, padding: 24, display: 'flex', flexDirection: 'row'}}>
+          <span>⬆️</span><span>{problem.upvotes}</span>
+        </div>
+        <div>
+          <h1>{problem.title}</h1>
+          <p>{problem.description}</p>
+        </div>
       </div>
     );
   }
