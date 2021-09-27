bb47238ae10905569dded1fb9957bae637b920b6
Bjørn-Olav Strand <bolav@hyre.no>
2021-09-27 21:43:18 +0200

    Add real data in views

--- src/api/problem.ts
+++ src/api/problem.ts
@@ -1,13 +1,25 @@
-import { getFirestore, collection, addDoc } from "firebase/firestore/lite";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore/lite";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../secrets.json";
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export async function createProblem(title: string, description : string) {
-    const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-  
     await addDoc(collection(db, "problems"), {
       title: title,
-      description: description
+      description: description,
+      upvotes: 0,
     });
+}
+
+export async function listProblems() {
+  const querySnapshot = await getDocs(collection(db, "problems"));
+  const docs = [];
+  for (const doc of querySnapshot.docs) {
+    docs.push({
+      id: doc.id,
+      data: doc.data(),
+    })
   }
+  return docs;
+}
