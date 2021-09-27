d8af2fc4917a1bf40c91c7bf8910861c220fa5ec
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2021-09-27 22:24:09 +0200

    Add frontend form for submitting a new question

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -1,11 +1,84 @@
 import { Link } from "react-router-dom";
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { colors } from "../colors";
 import { Problem } from "./ProblemView";
 import { listProblems } from "../api/problem";
 
+const CreateForm: React.FC = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div style={{ marginBottom: 64 }}>
+      <form
+        className="create-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert(title);
+          return false;
+        }}
+      >
+        <label
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "stretch",
+          }}
+        >
+          <div
+            style={{
+              marginBottom: 8,
+              color: "white",
+            }}
+          >
+            Title
+          </div>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            style={{ marginBottom: 16 }}
+          />
+        </label>
+        <label
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "stretch",
+          }}
+        >
+          <div
+            style={{
+              marginBottom: 8,
+              marginTop: 8,
+              color: "white",
+            }}
+          >
+            Description
+          </div>
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            style={{ marginBottom: 16 }}
+          />
+        </label>
+        <button
+          style={{
+            alignSelf: "flex-end",
+            fontSize: 16,
+            marginBottom: 16,
+            padding: "8px 16px",
+            marginTop: 8,
+          }}
+        >
+          Create
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export const Frontpage = () => {
   const [problems, setProblems] = useState<Problem[] | undefined>(undefined);
+  const [showCreateForm, setShowCreateForm] = useState(false);
 
   const getProblems = async () => {
     const problems = await listProblems();
@@ -13,9 +86,8 @@ export const Frontpage = () => {
   };
 
   useEffect(() => {
-    getProblems()
+    getProblems();
   }, []);
-
 
   return (
     <div className="content-wrapper">
@@ -27,73 +99,89 @@ export const Frontpage = () => {
         <em>Top problems, Top answers</em>
       </div>
 
+      {!showCreateForm && (
+        <button
+          className="btn"
+          style={{ fontSize: 16, padding: "8px 16px", marginBottom: 16 }}
+          onClick={(e) => {
+            e.preventDefault();
+            setShowCreateForm(true);
+          }}
+        >
+          Create new question
+        </button>
+      )}
+
+      {showCreateForm && <CreateForm />}
+
       <div>
-        {problems && problems.map((problem) => (
-          <div
-            style={{
-              marginBottom: 64,
-            }}
-          >
+        {problems &&
+          problems.map((problem) => (
             <div
               style={{
-                display: "flex",
-                alignItems: "center",
-                marginBottom: 16,
+                marginBottom: 64,
               }}
             >
               <div
                 style={{
-                  marginRight: 32,
-                  background: colors.gold,
-                  width: 64,
-                  height: 64,
                   display: "flex",
                   alignItems: "center",
-                  justifyContent: "center",
-                  borderRadius: 32,
-                  boxShadow: "4px 2px 0 " + colors.jade,
-                  flexShrink: 0,
+                  marginBottom: 16,
                 }}
               >
-                <div style={{ paddingLeft: 4 }}>{problem.data.upvotes} ↑</div>
+                <div
+                  style={{
+                    marginRight: 32,
+                    background: colors.gold,
+                    width: 64,
+                    height: 64,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 32,
+                    boxShadow: "4px 2px 0 " + colors.jade,
+                    flexShrink: 0,
+                  }}
+                >
+                  <div style={{ paddingLeft: 4 }}>{problem.data.upvotes} ↑</div>
+                </div>
+                <div
+                  style={{
+                    fontSize: 22,
+                    paddingRight: 16,
+                  }}
+                >
+                  <Link className="title-link" to={"/problems/" + problem.id}>
+                    {problem.data.title}
+                  </Link>
+                </div>
               </div>
               <div
                 style={{
-                  fontSize: 22,
-                  paddingRight: 16,
+                  paddingLeft: 64 + 32,
+                  paddingRight: 32,
+                  color: "#444",
+                  lineHeight: 1.5,
+                  maxHeight: 64 + 32 + 16,
+                  overflow: "hidden",
+                  position: "relative",
                 }}
               >
-                <Link className="title-link" to={"/problems/" + problem.id}>
-                  {problem.data.title}
-                </Link>
+                <div
+                  style={{
+                    position: "absolute",
+                    top: 32 + 16,
+                    background:
+                      "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
+                    left: 0,
+                    right: 0,
+                    height: 64,
+                  }}
+                />
+                {problem.data.description}
               </div>
             </div>
-            <div
-              style={{
-                paddingLeft: 64 + 32,
-                paddingRight: 32,
-                color: "#444",
-                lineHeight: 1.5,
-                maxHeight: 64 + 32 + 16,
-                overflow: "hidden",
-                position: "relative",
-              }}
-            >
-              <div
-                style={{
-                  position: "absolute",
-                  top: 32 + 16,
-                  background:
-                    "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
-                  left: 0,
-                  right: 0,
-                  height: 64,
-                }}
-              />
-              {problem.data.description}
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
